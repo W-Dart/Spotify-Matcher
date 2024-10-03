7f58157369efd5e2b551cd28a018c016e1755f6f
W-Dart <wjdart@wisc.edu>
2024-10-03 16:12:46 -0500

    changed base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Spotify-Matcher/', // Replace 'my-repo' with your actual repository name
+  base: './', // Replace 'my-repo' with your actual repository name
   plugins: [react()],
 })
